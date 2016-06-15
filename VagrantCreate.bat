5c62a8db2372e1abf38ce199816e467ae7939502
Gugli <code@gugli.me>
2016-06-15 20:44:44 +0200

    Better vagrant up script

--- VagrantCreate.bat
+++ VagrantCreate.bat
@@ -1,6 +1,16 @@
 @echo off
-SET LocalDir = %~dp0
+
+SET Putty="C:\Program Files (x86)\PuTTY\putty.exe"
+SET LocalDir=%~dp0%
 cd %LocalDir%
 vagrant up
+
+set SshPort=2222
+FOR /F "tokens=1-2 delims= " %%G IN ('vagrant ssh-config deploy') DO (
+    if "%%G"=="Port" set SshPort=%%H
+)
+echo "Port: %SshPort%"
+echo "Key: %LocalDir%VagrantInsecureKey.ppk"
+start "" /b %Putty% -ssh -P %SshPort% -i "%LocalDir%\VagrantInsecureKey.ppk" vagrant@127.0.0.1
 
 pause
